d95a05c4963a0f34570399833d225149b56db2c8
Steve Kane <quantumkane00@gmail.com>
2021-01-25 14:32:05 -0600

    mostly done

--- dataframes.py
+++ dataframes.py
@@ -5,8 +5,6 @@ np.random.seed(123)
 students = ['Sally', 'Jane', 'Suzie', 'Billy', 'Ada', 'John', 'Thomas',
             'Marie', 'Albert', 'Richard', 'Isaac', 'Alan']
 
-# randomly generate scores for each student for each subject
-# note that all the values need to have the same length here
 math_grades = np.random.randint(low=60, high=100, size=len(students))
 english_grades = np.random.randint(low=60, high=100, size=len(students))
 reading_grades = np.random.randint(low=60, high=100, size=len(students))
@@ -18,24 +16,24 @@ df = pd.DataFrame({'name': students,
 
 #a. Create a column named passing_english that indicates whether each student has a passing grade in english.
 
-df.assign(passing_english = df.english >= 70)
+df['passing_english'] = df.english > 70
 
 #b. Sort the english grades by the passing_english column. How are duplicates handled?
 
 df.sort_values(by='passing_english')
-# an error occurs
+# Not sure what the duplicates question is asking....?
 
 #c. Sort the english grades first by passing_english and then by student name. All the students that are failing english should be first, and within the students that are failing english they should be ordered alphabetically. The same should be true for the students passing english. (Hint: you can pass a list to the .sort_values method)
 
-
+df.sort_values(by = ['passing_english', 'name'])
 
 #d. Sort the english grades first by passing_english, and then by the actual english grade, similar to how we did in the last step.
 
-
+df.sort_values(by = ['passing_english', 'english'])
 
 #e. Calculate each students overall grade and add it as a column on the dataframe. The overall grade is the average of the math, english, and reading grades.
 
-
+df['overall_grade'] = df['math'] + df['english'] + df['reading'] / 3
 
 
 
@@ -68,7 +66,7 @@ mpg.columns
 
 #f. Do any cars have better city mileage than highway mileage?
 
-
+mpg[mpg.city > mpg.highway]
 
 #g. Create a column named mileage_difference this column should contain the difference between highway and city mileage for each car.
 
@@ -77,7 +75,7 @@ mpg.head()
 
 #h Which car (or cars) has the highest mileage difference?
 
-
+mpg.sort_values(by='mileage_difference', ascending=False)
 
 #i Which compact class car has the lowest highway mileage? The best?
 
@@ -85,6 +83,38 @@ mpg.head()
 
 #j Create a column named average_mileage that is the mean of the city and highway mileage.
 
-
+mpg['average_mileage'] = (mpg['highway'] + mpg['city']) / 2
+mpg.head()
 
 #l Which Dodge car has the best average mileage? The worst?
+
+
+
+
+##3 Load the Mammals dataset. Read the documentation for it, and use the data to answer these questions:
+
+mammals = data('Mammals')
+
+#a. How many rows and columns are there?
+
+mammals.info()
+
+#b. What are the data types?
+
+mammals.info()
+
+#c. Summarize the dataframe with .info and .describe
+
+mammals.info()
+
+mammals.describe()
+
+#d. What is the the weight of the fastest animal?
+
+mammals.sort_values(by = 'speed', ascending=False)
+
+#e. What is the overall percentage of specials?
+
+
+
+#f. How many animals are hoppers that are above the median speed? What percentage is this?
