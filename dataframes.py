9e9f7a800bd83a1bd6b088a27b102bb7aa4d1e70
Steve Kane <quantumkane00@gmail.com>
2021-01-25 11:48:03 -0600

    working on it

---
+++ dataframes.py
@@ -0,0 +1,31 @@
+
+
+
+#2 Load the mpg dataset. Read the documentation for the dataset and use it for the following questions:
+
+#a. How many rows and columns are there?
+
+data('mpg', show_doc=True) 
+234 rows and 11 columns
+
+#b. What are the data types of each column?
+
+
+#c. Summarize the dataframe with .info and .describe
+
+
+#d. Rename the cty column to city.
+
+
+#e. Rename the hwy column to highway.
+
+
+#f. Do any cars have better city mileage than highway mileage?
+
+#g. Create a column named mileage_difference this column should contain the difference between highway and city mileage for each car.
+
+
+#h Which car (or cars) has the highest mileage difference?
+#i Which compact class car has the lowest highway mileage? The best?
+#j Create a column named average_mileage that is the mean of the city and highway mileage.
+#l Which Dodge car has the best average mileage? The worst?
