cf642eee8c677db523ae31076a308c21666bd20f
Steve Kane <quantumkane00@gmail.com>
2021-01-27 15:42:28 -0600

    almost done2

--- advanced_dataframes.py
+++ advanced_dataframes.py
@@ -7,7 +7,7 @@ mpg = data('mpg')
 
 #a. On average, which manufacturer has the best miles per gallon?
 
-mpg.sort_values(by = 'average_mileage', ascending = False).head()
+mpg.groupby('manufacturer').average_mileage.mean().nlargest(1, keep='all')
 
 #b. How many different manufacturers are there?
 
@@ -54,19 +54,21 @@ pd.merge(users, roles, left_on='role_id', right_on='id', how='outer')
 
 # What happens if you drop the foreign keys from the dataframes and try to merge them?
 
+# Confusion happens
 
 ##3 Getting data from SQL databases
 
 #a. Create a function named get_db_url. It should accept a username, hostname, password, and database name and return a url formatted like in the examples in this lesson.
 
 get_db_url = f'mysql+pymysql://{user}:{password}@{host}/employees'
+    
 #b. Use your function to obtain a connection to the employees database.
 
 query = """
     SELECT * FROM employees limit 5
 """
 
-pd.read_sql(query, url)
+employees = pd.read_sql(query, url)
 
 #c. Once you have successfully run a query:
 #Intentionally make a typo in the database url. What kind of error message do you see?
@@ -82,12 +84,12 @@ ProgrammingError: (pymysql.err.ProgrammingError) (1146, "Table 'employees.employ
 query = """
     SELECT * FROM employees limit 5
 """
-pd.read_sql(query, get_db_url)
+employees = pd.read_sql(query, get_db_url)
 
 query = """
     SELECT * FROM titles limit 5
 """
-pd.read_sql(query, get_db_url)
+titles = pd.read_sql(query, get_db_url)
 
 #e. Visualize the number of employees with each title.
 
@@ -95,7 +97,8 @@ pd.read_sql(query, get_db_url)
 
 #f. Join the employees and titles dataframes together.
 
-pd.merge(employees, titles, on='emp_no')
+emp_title = pd.merge(employees, titles, on='emp_no')
+emp_title.head()
 
 #g. Visualize how frequently employees change titles.
 
@@ -103,8 +106,7 @@ pd.merge(employees, titles, on='emp_no')
 
 #h. For each title, find the hire date of the employee that was hired most recently with that title.
 
-
-
+all_emp_titles.groupby('title').hire_date.max()
 
 #i.Write the code necessary to create a cross tabulation of the number of titles by department. (Hint: this will involve a combination of SQL and python/pandas code)
 
@@ -116,5 +118,6 @@ pd.merge(employees, titles, on='emp_no')
 
 #b. What are the most popular 3 items?
 
+chipotle['item_name'].value_counts().head(3)
 
 #c. Which item has produced the most revenue?
