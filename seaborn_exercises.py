461eaf2e6e27e6fe6675ae3bf1ade936d25a2cd3
Steve Kane <quantumkane00@gmail.com>
2021-01-28 09:44:27 -0600

    in progress

---
+++ seaborn_exercises.py
@@ -0,0 +1,68 @@
+
+# Use the iris database to answer the following quesitons:
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+get_db_url = f'mysql+pymysql://{user}:{password}@{host}/iris_db'
+    
+query = """
+    SELECT * FROM measurements limit 10
+"""
+iris_measure = pd.read_sql(query, get_db_url)
+iris_measure
+
+# What does the distribution of petal lengths look like?
+
+sns.distplot(iris_measure.petal_length)
+
+# Is there a correlation between petal length and petal width?
+
+sns.lmplot(x='petal_length', y='petal_width', data=iris_measure)
+plt.title('Petal Length vs Petal Width')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+
+# Would it be reasonable to predict species based on sepal width and sepal length?
+
+
+
+# Which features would be best used to predict species?
+
+
+
+##1 Using the lesson as an example, use seaborn's load_dataset function to load the anscombe data set. Use pandas to group the data by the dataset column, and calculate summary statistics for each dataset. What do you notice?
+
+# Plot the x and y values from the anscombe data. Each dataset should be in a separate column.
+
+
+##2 Load the InsectSprays dataset and read it's documentation. Create a boxplot that shows the effectiveness of the different insect sprays.
+
+
+
+##3 Load the swiss dataset and read it's documentation. Create visualizations to answer the following questions:
+
+
+
+# Create an attribute named is_catholic that holds a boolean value of whether or not the province is Catholic. (Choose a cutoff point for what constitutes catholic)
+
+
+
+# Does whether or not a province is Catholic influence fertility?
+
+
+
+# What measure correlates most strongly with fertility?
+
+
+
+##4 Using the chipotle dataset from the previous exercise, create a bar chart that shows the 4 most popular items and the revenue produced by each.
+
+
+
+##5 Load the sleepstudy data and read it's documentation. Use seaborn to create a line chart of all the individual subject's reaction times and a more prominant line showing the average change in reaction time.
+
+
+
